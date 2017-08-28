92ad57ac60a1bc8819fd64a61a891bc9360b21f9
showeimer <showeimer@gmail.com>
2017-08-28 14:41:53 -0400

    initial commit, setting up project files

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+
+import APIKEY from './apiKey.js';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default App;
